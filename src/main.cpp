4ed79c8926d82b73d5392c2eab8301d9d9aac94f
paufau <pakseev.1999@gmail.com>
2025-08-17 16:50:48 +0400

    Remove old discovery & server

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,7 @@
-#include "flows/receiver/receiver.h"
-#include "flows/sender/sender.h"
 #include "gui/framework/ui_window.h"
 #include "gui/scenes/home/home_scene.h"
 #include "gui/scenes/receiver/receiver_scene.h"
-#include "keyboard/input_event.h"
-#include "keyboard/keyboard.h"
-#include "networking/discovery/discovery.h"
-#include "networking/server/server.h"
 #include "store.h"
-#include "utils/cli/args.h"
-#include "utils/get_platform/platform.h"
-
-#include <atomic>
-#include <chrono>
-#include <iostream>
-#include <mutex>
-#include <sstream>
-#include <string>
-#include <thread>
 
 int main(int argc, char* argv[])
 {
