7d18d85f059db047c792e620b4ed3cb7437045fd
paufau <pakseev.1999@gmail.com>
2025-08-17 21:57:16 +0400

    Split Session & SessionServer headers and declarations

--- src/networking/p2p/SessionServer.h
+++ src/networking/p2p/SessionServer.h
@@ -16,27 +16,11 @@ class SessionServer {
 public:
   using NewSessionHandler = std::function<void(Session::Ptr)>;
 
-  SessionServer(asio::io_context& io, uint16_t port = 0) : io_(io), acceptor_(io) {
-    asio::ip::tcp::endpoint ep(asio::ip::tcp::v4(), port);
-    acceptor_.open(ep.protocol());
-    acceptor_.set_option(asio::ip::tcp::acceptor::reuse_address(true));
-    acceptor_.bind(ep);
-    acceptor_.listen();
-  }
+  SessionServer(asio::io_context& io, uint16_t port = 0);
 
-  uint16_t port() const { return acceptor_.local_endpoint().port(); }
+  uint16_t port() const;
 
-  void async_accept(NewSessionHandler on_new) {
-    auto sock = std::make_shared<asio::ip::tcp::socket>(io_);
-    acceptor_.async_accept(*sock,
-      [this, sock, on_new](std::error_code ec) {
-        if (!ec) {
-          auto session = std::make_shared<Session>(std::move(*sock));
-          if (on_new) on_new(session);
-        }
-        async_accept(on_new);
-      });
-  }
+  void async_accept(NewSessionHandler on_new);
 
 private:
   asio::io_context& io_;
