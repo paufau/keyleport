666048051623dd7f57c2623d0cf8dfa31066f61e
paufau <pakseev.1999@gmail.com>
2025-09-05 21:13:23 +0400

    Service location

--- src/services/services_repository.h
+++ src/services/services_repository.h
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <memory>
+#include <type_traits>
 #include <vector>
 
 namespace services
@@ -22,6 +23,22 @@ namespace services
     const std::vector<stored_service_t>& get_services() const
     {
       return services_;
+    }
+
+    template <typename T> std::shared_ptr<T> get_service() const
+    {
+      static_assert(
+          std::is_base_of<services::service_lifecycle_listener, T>::value,
+          "T must derive from services::service_lifecycle_listener");
+
+      for (const auto& svc : services_)
+      {
+        if (auto casted = std::dynamic_pointer_cast<T>(svc))
+        {
+          return casted;
+        }
+      }
+      return nullptr;
     }
 
     void remove_service(stored_service_t svc)
